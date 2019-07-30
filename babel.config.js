b35226f3053a8fff22eed90627c534de44e068c8
Lukas Horak <horaklukas@centrum.cz>
2019-07-30 10:42:49 +0200

    Add babel transform-imports plugin to prevent full imports from lodash

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  presets: ["@babel/typescript", "@babel/env"],
-  plugins: [
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread"
-  ],
-  ignore: ["**/__tests__/", "packages/**/*.d.ts"]
+    presets: ['@babel/typescript', '@babel/env'],
+    plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        [
+            require.resolve('babel-plugin-transform-imports'),
+            {
+                lodash: {
+                    transform: 'lodash/${member}',
+                    preventFullImport: true,
+                },
+            },
+        ],
+    ],
+    ignore: ['**/__tests__/', 'packages/**/*.d.ts'],
 };
