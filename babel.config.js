31f36b5609b471129141cf1a44e7744e5eed6acd
Lukas Horak <horaklukas@centrum.cz>
2019-07-30 09:52:09 +0200

    Add build scripts, setup compilation, remove useless packages

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    presets: ['@babel/typescript', '@babel/env', '@babel/react'],
-    plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
-    ignore: ['**/__tests__/', 'src/**/*.d.ts'],
+  presets: ["@babel/typescript", "@babel/env"],
+  plugins: [
+    "@babel/proposal-class-properties",
+    "@babel/proposal-object-rest-spread"
+  ],
+  ignore: ["**/__tests__/", "packages/**/*.d.ts"]
 };
