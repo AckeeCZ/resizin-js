a2ee8b38def1194a46062bb5448ab61ec5d873f3
Lukas Horak <horaklukas@centrum.cz>
2019-10-07 17:15:45 +0200

    Make buildUrlFactory a default export from resizin package

--- packages/resizin/src/buildUrl/buildUrlFactory.ts
+++ packages/resizin/src/buildUrl/buildUrlFactory.ts
@@ -8,14 +8,16 @@ export interface ClientOptions {
     bucket: string;
 }
 
-const buildUrlFactory = (options: ClientOptions) => (imageId: string, buildOptions?: Options) => {
+const buildUrlFactory = (options: ClientOptions) => {
     if (!options.bucket) {
-        throw new Error('You did not provide bucket name, which is required for building iamge url');
+        throw new Error('You did not provide bucket name, which is required for building image url');
     }
 
     const serverUrl = options.serverUrl || DEFAULT_SERVER_URL;
 
-    buildUrl(serverUrl, options.bucket, imageId, buildOptions);
+    return (imageId: string, buildOptions?: Options) => {
+        buildUrl(serverUrl, options.bucket, imageId, buildOptions);
+    };
 };
 
 export default buildUrlFactory;
