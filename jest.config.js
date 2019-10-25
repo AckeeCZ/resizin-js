2b9ac0aacfaeee101f8ad24fa03e7ed441cdb7d7
Lukas Horak <horaklukas@centrum.cz>
2019-10-25 12:51:27 +0200

    :bug: Fix extending expect matchers

--- jest.config.js
+++ jest.config.js
@@ -13,13 +13,13 @@ const sharedSettings = {
 
 module.exports = {
     testPathIgnorePatterns: ['/node_modules', '/lib'],
-    setupFilesAfterEnv: ['./configure-jest.ts'],
     // https://gist.github.com/thebuilder/15a084f74b1c6a1f163fc6254ad5a5ba
     projects: [
         {
             displayName: 'dom',
             testEnvironment: 'jsdom',
             testMatch: ['**/packages/!(node-*)/**/__tests__/**/*.test.ts?(x)'],
+            setupFilesAfterEnv: ['<rootDir>/configure-jest.ts'],
             ...sharedSettings,
         },
         {
