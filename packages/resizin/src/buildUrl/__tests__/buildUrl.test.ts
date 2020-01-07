52c3f57e86b229659169f8ad59294d1b4d1bae0a
Lukas Horak <horaklukas@centrum.cz>
2020-01-07 16:46:28 +0100

    🐛Fix doubled slash at image url when imageId starts with slash

--- packages/resizin/src/buildUrl/__tests__/buildUrl.test.ts
+++ packages/resizin/src/buildUrl/__tests__/buildUrl.test.ts
@@ -4,7 +4,7 @@ import { serializeOptions } from '../../options';
 jest.mock('../../options');
 
 describe('Build source url', () => {
-    it('use append slash to serialized options and make the url', () => {
+    it('append slash to serialized options and make the url', () => {
         (serializeOptions as jest.Mock).mockReturnValue('serialized-options-mock');
         expect(buildUrl('base', 'test-bucket', 'image1', { width: 14, crop: 'fill' })).toEqual(
             'base/test-bucket/image/serialized-options-mock/image1',
@@ -16,4 +16,19 @@ describe('Build source url', () => {
 
         expect(buildUrl('base', 'test-bucket', 'image2', {})).toEqual('base/test-bucket/image/image2');
     });
+
+    it('should prevent doubled slashes when image id starts with slash and there are options', () => {
+        (serializeOptions as jest.Mock).mockReturnValue('serialized-options-mock');
+        expect(buildUrl('base', 'test-bucket', '/image3', { width: 14, crop: 'fill' })).toEqual(
+            'base/test-bucket/image/serialized-options-mock/image3',
+        );
+    });
+
+    it('should prevent doubled slashes when image id starts with slash and there are no options', () => {
+        (serializeOptions as jest.Mock).mockReturnValue('');
+        expect(buildUrl('base', 'test-bucket', '/image4' )).toEqual(
+            'base/test-bucket/image/image4',
+        );
+    });
+
 });
--- packages/resizin/src/buildUrl/buildUrl.ts
+++ packages/resizin/src/buildUrl/buildUrl.ts
@@ -2,9 +2,14 @@ import { serializeOptions, Options } from '../options';
 
 const buildUrl = (serverUrl: string, bucket: string, imageId: string, options?: Options) => {
     let serializedOptions = serializeOptions(options);
+
     if (serializedOptions) {
         serializedOptions += '/';
     }
+    if (imageId.charAt(0) === '/') {
+        imageId = imageId.substr(1);
+    }
+
     return `${serverUrl}/${bucket}/image/${serializedOptions}${imageId}`;
 };
 
