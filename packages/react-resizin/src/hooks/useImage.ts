e476d8ca90e457de983a981fb5bec4fbc52814fc
Lukas Horak <horaklukas@centrum.cz>
2022-04-06 10:13:56 +0200

    🏷️ Allow passing falsy value as an imageId to useImage hook

--- packages/react-resizin/src/hooks/useImage.ts
+++ packages/react-resizin/src/hooks/useImage.ts
@@ -2,7 +2,7 @@ import { Options } from 'resizin';
 
 import useResizinContext from './useResizinContext';
 
-const useImage = (imageId: string, options?: Options): string|null => {
+const useImage = (imageId?: string | null, options?: Options): string | null => {
     const buildImageUrl = useResizinContext();
 
     return imageId ? buildImageUrl(imageId, options) : null;
